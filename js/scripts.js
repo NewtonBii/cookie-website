c67eb36acf9b655b2ee26f37677a2d7ec8254c29
Newton Bii <biinewton382@gmail.com>
2017-10-17 14:46:11 +0300

    Add JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $(".button-first").click(function() {
+    $("body").removeClass();
+    $("body").addClass("first-background");
+  });
+
+  $(".button-second").click(function() {
+    $("body").removeClass();
+    $("body").addClass("second-background");
+  });
+
+  $(".button-third").click(function() {
+    $("body").removeClass();
+    $("body").addClass("third-background");
+  });
+
+  $(".button-fourth").click(function() {
+    $("body").removeClass();
+    $("body").addClass("fourth-background");
+  });
+
+  $(".button-original").click(function() {
+    $("body").removeClass();
+    $("body").addClass("original-background");
+  });
+
+});
